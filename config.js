d2e29e2e21afbe43191acb4ad5af8162f5a59d82
Nizar Khalife Iglesias <nizar@nobox.com>
2013-08-25 09:55:03 -0400

    Add Regis To Go URL processing

--- config.js
+++ config.js
@@ -1,8 +1,23 @@
-var sendgridUrl = process.env.SENDGRID_URL || 'user:password'
+var url = require('url')
+    , sendgridUrl = process.env.SENDGRID_URL || 'user:password'
     , sendgridCredentials = sendgridUrl.split(':')
+    , redisUrl = process.env.REDISTOGO_URL
+    , redistogo
+    , auth
 ;
 
 module.exports.SENDGRID = {
     username: sendgridCredentials[0]
     , password: sendgridCredentials[1]
 };
+
+if (!redisUrl) {
+    module.exports.REDISTOGO = false;
+} else {
+    redistogo = url.parse(redisUrl);
+    module.exports.REDISTOGO = {
+        port: redistogo.port
+        , hostname: redistogo.hostname
+        , auth: redistogo.auth.split(':')[1]
+    };
+}
--- db.js
+++ db.js
@@ -1,7 +1,16 @@
-var redis = require('redis');
+var redis = require('redis')
+    , config = require('./config')
+;
 
 function createClient () {
-    return redis.createClient();
+    var client;
+    if (!config.REDISTOGO) {
+         client = redis.createClient();
+    } else {
+        client = redis.createClient(config.REDISTOGO.port, config.REDISTOGO.hostname);
+        client.auth(config.REDISTOGO.auth);
+    }
+    return client;
 }
 
 module.exports.createClient = createClient;
